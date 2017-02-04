2e86045fa075e9e8bcc188db25a64ec6f2c50f3b
neiljohnson <mr.neil.j@gmail.com>
2017-02-04 15:19:51 -0800

    loaded primary struct main function typed up

--- main.cpp
+++ main.cpp
@@ -12,3 +12,39 @@ struct UserInfo
     int age;
     char gender;
 };
+
+void placeCursor(HANDLE, int, int);
+void displayPrompts(HANDLE);
+void getUserInput(HANDLE, userInfo&);
+void displayData(HANDLE, userInfo);
+
+int main()
+{
+    userInfo input;              // input is a UserInfo structure
+                                // that has 3 member variables
+
+    // Get the handle to standard output device (the consol)
+    HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    displayPrompts(screen);
+    getUserInput(screen, input);
+    displayData(screen, input);
+
+    return 0;
+}
+
+/************************************************************
+ *                      placeCursor                         *
+ ************************************************************/
+void placeCursor(HANDLE screen, int row, int col)
+{
+    COORD positin;
+    position.X = row;
+    position.Y = col;
+    SetConsoleCursorPosition(screen, position);
+}
+
+
+/************************************************************
+ *                      displayPrompts                      *
+ ************************************************************/
